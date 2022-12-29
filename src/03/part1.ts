8c5dcc7378cd5eb513ae53faf44abf5a427a618a
Nick Warters <nick.warters@gmail.com>
2022-12-29 20:59:07 +0000

    Day 3 Part 1

--- src/03/part1.ts
+++ src/03/part1.ts
@@ -1,6 +1,9 @@
 import { readFileSync } from 'fs'
+import { splitLines } from '../utils/helpers.js'
 
-const tests: [string, any][] = [[``, 0]]
+const tests: [string, any][] = [[`#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2`, 4]]
 
 tests.forEach(([input, expected], i) => {
     const result = solve(input)
@@ -12,11 +15,32 @@ Got: ${result}
 Test ${i + 1} ${result === expected ? 'Pass': 'Fail'}`)
 })
 
-//console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
+console.log(`Full Input Solution\n----\n${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}`)
 
 function solve(input: string): number {
 
     let retVal = 0
+
+    const lines = splitLines(input)
+
+    const coords: { [key: string]: number } = {}
+
+    for (const line of lines) {
+        const [id, x, y, width, height] = line.match(/\d+/g)!.map(n => parseInt(n))
+
+        for(let h = 0; h < height; h++){
+            for(let w = 0; w < width; w++){
+                const c = [x + w, y + h].toString()
+                if(coords[c] === undefined){
+                    coords[c] = 0
+                }
+
+                coords[c]++
+            }
+        }
+    }
+
+    retVal = Object.keys(coords).filter(c => coords[c] > 1).length
 
     return retVal
 }
